1affe19365b6912130486bc367c5c116f32c23ef
Bayu <bhentbayu@gmail.com>
2025-01-02 10:08:08 +0900

    Menambahkan rute batu (/, blog, about, contact)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
